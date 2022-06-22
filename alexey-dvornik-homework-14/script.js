24952f8befc6558349938a055c90e4e4eed9aade
Oleksii Dvornik <alexey.dvronik@gmail.com>
2022-06-22 11:04:40 +0300

    Homework #14

--- alexey-dvornik-homework-14/script.js
+++ alexey-dvornik-homework-14/script.js
@@ -2,9 +2,13 @@ const prevButton = $('#prev');
 const nextButton = $('#next');
 
 nextButton.on('click', function () {
-    $('p#first').removeAttr('id').next('p').attr('id','first');
-});
+    if ( ($('#first').next('p')).length !== 0 ) {
+        $('p#first').removeAttr('id').next('p').attr('id','first');
+    }
+})
 
 prevButton.on('click', function () {
-    $('p#first').removeAttr('id').prev('p').attr('id','first');
-});
+    if ( ($('#first').prev('p')).length !== 0 ) {
+        $('p#first').removeAttr('id').prev('p').attr('id','first');
+    }
+})
