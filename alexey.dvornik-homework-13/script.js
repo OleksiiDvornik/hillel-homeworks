8d167a8f7ff9b8faa5973cba9ccd5d3342cc0e35
Oleksii Dvornik <alexey.dvronik@gmail.com>
2022-06-23 22:34:55 +0300

    Homework #13

---
+++ alexey.dvornik-homework-13/script.js
@@ -0,0 +1,38 @@
+function Clock () {
+    this.timer = undefined;
+
+    this.start = function () {
+        this.timer = setInterval(this.timeRender, 1000);
+    }
+
+    this.timeRender = function () {
+        const date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        document.querySelector(".js--clock").innerHTML = `${hours} : ${minutes} : ${seconds}`;
+    }
+}
+
+const clock = new Clock();
+
+Clock.prototype.stop = function () {
+    clearInterval(this.timer);
+}
+
+document.querySelector('.js--start').addEventListener('click', () => {
+    clock.start();
+});
+
+document.querySelector('.js--stop').addEventListener('click', () => {
+    clock.stop();
+});
