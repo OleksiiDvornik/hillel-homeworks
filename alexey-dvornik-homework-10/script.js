bb8c75f24ed5af4860e57e0a016f69af7aecca17
Oleksii Dvornik <alexey.dvronik@gmail.com>
2022-06-25 23:27:32 +0300

    Homework #10

--- alexey-dvornik-homework-10/script.js
+++ alexey-dvornik-homework-10/script.js
@@ -1,25 +1,71 @@
 function UserTable (params) {
-    this.init = function() {
+
+    this.init = function () {
         params.form.addEventListener('submit', (event) => {
-            event.preventDefault();
-            this.addUser();
+            this.formSubmit(event);
+        })
+        params.add.addEventListener('click', function () {
+            params.form.classList.add('open');
         })
     }
-    this.addUser = function() {
-        this.userTemplate({
-            id: Math.floor(Math.random() * 100),
-            name: params.form['name'],
-            phone: '0631234554',
-            age: 33
-        })
+
+    this.formSubmit = function (event) {
+        event.preventDefault();
+        const currentID = params.form.elements['id'].value;
+        if (currentID) {
+            this.updateUser({
+                id: params.form['id'].value,
+                name: params.form['name'].value,
+                phone: params.form['phone'].value,
+                age: params.form['age'].value,
+            });
+            params.form.classList.remove('open')
+        } else {
+            this.addUser({
+                id: Math.floor(Math.random() * 100),
+                name: params.form['name'].value,
+                phone: params.form['phone'].value,
+                age: params.form['age'].value,
+            });
+        }
+    }
+
+    this.addUser = function (user) {
+        this.userTemplate(user);
+        const users = JSON.parse(localStorage.getItem('users'));
+        const currentUsers = users || [];
+        currentUsers.push(user);
+        localStorage.setItem('users', JSON.stringify(currentUsers));
         params.form.reset();
     }
-    this.updateUser = function() {
 
+    this.updateUser = function (user) {
+        const users = JSON.parse(localStorage.getItem('users'));
+        const newUsers = users.map(item => {
+            if (item.id === Number(user.id)) {
+                return user;
+            }
+            return item;
+        });
+        params.content.innerHTML = '';
+        newUsers.forEach((newUser) => {
+            this.userTemplate(newUser);
+        })
+        localStorage.setItem('users', JSON.stringify(newUsers));
     }
-    this.userTemplate = function(user) {
+
+    this.loadUser = function () {
+        const users = JSON.parse(localStorage.getItem('users'));
+        if (users) {
+            users.forEach(user => {
+                this.userTemplate(user);
+            })
+        }
+    }
+
+    this.userTemplate = function (user) {
         params.content.insertAdjacentHTML('beforeend', (
-            `<tr>
+            `<tr data-id="${user.id}">
                 <td>${user.id}</td>
                 <td>${user.name}</td>
                 <td>${user.phone}</td>
@@ -31,14 +77,53 @@ function UserTable (params) {
                 </td>
             </tr>`
         ))
+
+        const currentElement = document.querySelector(`[data-id="${user.id}"]`);
+        const viewButton = currentElement.querySelector('.js--view');
+        const editButton = currentElement.querySelector('.js--edit');
+        const deleteButton = currentElement.querySelector('.js--delete');
+
+        const editUser = function () {
+            const form = params.form;
+            form.classList.add('open');
+            form.elements['id'].value = user.id;
+            form.elements['name'].value = user.name;
+            form.elements['phone'].value = user.phone;
+            form.elements['age'].value = user.age;
+        }
+
+        const showUser = function () {
+            params.user.innerHTML = JSON.stringify(user, undefined, 2);
+        }
+
+        const deleteUser = () => {
+            const users = JSON.parse(localStorage.getItem('users'));
+            const newUsers = [];
+            users.forEach(item => {
+                if (item.id !== user.id) {
+                    newUsers.push(item);
+                }
+            })
+            params.content.innerHTML = '';
+            newUsers.forEach((newUser) => {
+                this.userTemplate(newUser);
+            })
+            localStorage.setItem('users', JSON.stringify(newUsers));
+        }
+
+        viewButton.addEventListener('click', showUser);
+        editButton.addEventListener('click', editUser);
+        deleteButton.addEventListener('click', deleteUser);
     }
+
+    this.loadUser();
 }
 
 const params = {
     content: document.querySelector('.js--content'),
     add: document.querySelector('.js--add'),
     form: document.querySelector('.js--form'),
-    user: document.querySelector('.js--user')
+    user: document.querySelector('.js--user'),
 }
 
 const userTable = new UserTable(params);
--- alexey-dvornik-homework-13/class.js
+++ alexey-dvornik-homework-13/class.js
@@ -1,25 +1,51 @@
+// class Clock {
+//     constructor() {
+//         this.timer = undefined;
+//         this.start = function () {
+//             this.timer = setInterval(this.timeRender, 1000);
+//         }
+//         this.timeRender = function () {
+//             const date = new Date();
+//             let hours = date.getHours();
+//             let minutes = date.getMinutes();
+//             let seconds = date.getSeconds();
+//             if (hours < 10) {
+//                 hours = "0" + hours;
+//             }
+//             if (minutes < 10) {
+//                 minutes = "0" + minutes;
+//             }
+//             if (seconds < 10) {
+//                 seconds = "0" + seconds;
+//             }
+//             document.querySelector(".js--clock").innerHTML = `${hours} : ${minutes} : ${seconds}`;
+//         }
+//     }
+//     stop () {
+//         clearInterval(this.timer);
+//     }
+// }
+
 class Clock {
-    constructor() {
-        this.timer = undefined;
-        this.start = function () {
-            this.timer = setInterval(this.timeRender, 1000);
+    timer = undefined;
+    start () {
+        this.timer = setInterval(this.timeRender, 1000)
+    }
+    timeRender () {
+        const date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        if (hours < 10) {
+            hours = "0" + hours;
         }
-        this.timeRender = function () {
-            const date = new Date();
-            let hours = date.getHours();
-            let minutes = date.getMinutes();
-            let seconds = date.getSeconds();
-            if (hours < 10) {
-                hours = "0" + hours;
-            }
-            if (minutes < 10) {
-                minutes = "0" + minutes;
-            }
-            if (seconds < 10) {
-                seconds = "0" + seconds;
-            }
-            document.querySelector(".js--clock").innerHTML = `${hours} : ${minutes} : ${seconds}`;
+        if (minutes < 10) {
+            minutes = "0" + minutes;
         }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        document.querySelector(".js--clock").innerHTML = `${hours} : ${minutes} : ${seconds}`;
     }
     stop () {
         clearInterval(this.timer);
